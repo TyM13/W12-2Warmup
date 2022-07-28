3818214f26121fe29bdfdea9fb74cd8887c7a27f
Your Name <you@example.com>
2022-07-28 17:06:56 -0600

    W12-2 warmup

--- js/app.js
+++ js/app.js
@@ -1,14 +1,55 @@
-function linkSuccess(response){
-    document.body.insertAdjacentHTML(`afterbegin`, `<h1>loaded successfully</h1>`)
-}
+// function clickbutton (details) {
+// axios.request({
+//    url: `https://picsum.photos/200/300`
+// }).then(linkSuccess).catch(linkFailure)
+// }
+
+
+// function linkSuccess(response){
+//     document.body.insertAdjacentHTML(`afterbegin`, `<section>
+
+//     <img src="${response[`request`][`responseURL`]}">
+    
+//         </section>`)
+    
+// }
 
 
 
-function linkFailure(error){
-    document.body.insertAdjacentHTML(`afterbegin`, `<h1>loaded successfully</h1>`)
-}
+// function linkFailure(error){
+//     document.body.insertAdjacentHTML(`afterbegin`, `<h1>Error</h1>`)
+// }
 
 
-axios.request({
-   url: `https://picsum.photos/200/300`
-}).then().catch()
+// let button_one = document.getElementById(`button_one`)
+// button_one.addEventListener(`click`, clickbutton)
+
+
+
+
+
+function clickbuttontwo (details) {
+    axios.request({
+       url: `https://picsum.photos/200/300`
+    }).then(linkSuccesstwo).catch(linkFailuretwo)
+    }
+    
+    
+    function linkSuccesstwo(response){
+        document.body.insertAdjacentHTML(`afterbegin`, `<section>
+        
+        <img src="${response[`request`][`responseURL`]}">
+        
+            </section>`)
+        
+    }
+    
+    
+    
+    function linkFailuretwo(error){
+        document.body.insertAdjacentHTML(`afterbegin`, `<h1>Error</h1>`)
+    }
+    
+    
+    let button_two = document.getElementById(`button_two`)
+    button_two.addEventListener(`click`, clickbuttontwo)
